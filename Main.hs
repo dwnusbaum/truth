c679c3f5db012ff5320ea39ae85d9d901499828c
Devin Nusbaum <d.w.nusbaum@gmail.com>
2013-08-31 23:47:59 -0400

    Full truth table is now written to a csv file, which can be specified as a command line argument

--- Main.hs
+++ Main.hs
@@ -10,17 +10,26 @@ import System.Environment (getArgs)
 import Truth
 import Parse
 
+parseAndEval :: String -> String -> IO ()
+parseAndEval input filename = case parse input of
+    Left err -> print err
+    Right rules -> putStrLn (showResults (filter (\(_,y) -> all (==True) y) evaled)) >> writeFile filename (csv rules evaled) >> putStrLn ("Truth table written to " ++ filename)
+      where evaled = uncurry eval $ varSplit rules
+
 showResults :: [([(String,Bool)],[Bool])] -> String
 showResults [] = "No values satisfied all rules"
-showResults vals = "The following values satisfied all rules:\n" ++ intercalate "\nand\n" (map (unwords . map ppBool . fst) vals)
-  where ppBool (s, True) = s
-        ppBool (s,False) = '~' : s
+showResults vals = "The following values satisfied all rules:\n" ++ intercalate "\nand\n" (map (unwords . map showVar . fst) vals)
+  where showVar (s,True ) = s
+        showVar (s,False) = '~' : s
+
+csv :: [Term] -> [([(String,Bool)],[Bool])] -> String
+csv rules vals = unlines $ intercalate "," (map show rules) : map (\(x,y) -> intercalate "," (map (showBool . snd) x ++ map showBool y)) vals
+  where showBool True  = "T"
+        showBool False = "F"
 
 main :: IO ()
 main = getArgs >>= \args -> case args of
     [] -> putStrLn "No rules specified. Example usage: truth \"A;B;A|~B\""
-    [x] -> case parse x of
-        Left err -> print err
-        Right rules -> putStrLn (showResults (filter (\(_,y) -> all (==True) y) evaled))
-          where evaled = uncurry eval $ varSplit rules
+    [input]          -> parseAndEval input "truth.csv"
+    [input,filename] -> parseAndEval input filename
     _ -> putStrLn "Invalid command line arguments"
